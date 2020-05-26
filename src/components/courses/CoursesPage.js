5bddc195148ed7b14843d0a6e8b67d56941cb0c3
Dillon Coffman <tug40394@temple.edu>
2020-05-26 15:04:03 -0400

    Initial app structure

---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CoursesPage extends Component {
+  render() {
+    return (
+      <h2>
+        Courses
+      </h2>
+    )
+  }
+}
