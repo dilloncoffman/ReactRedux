aa00a7be6856ea0a26cba7c0253c8fe3315b467a
Dillon Coffman <tug40394@temple.edu>
2020-05-27 15:51:50 -0400

    Added initialState as default param in reducers, this helps developers understand the Redux store shape

--- src/redux/reducers/authorReducer.js
+++ src/redux/reducers/authorReducer.js
@@ -1,6 +1,7 @@
 import * as types from '../actions/actionTypes';
+import initialState from '../initialState';
 
-export default function authorReducer(state = [], action) {
+export default function authorReducer(state = initialState.authors, action) {
   switch (action.type) {
     case types.LOAD_AUTHORS_SUCCESS:
       return action.authors; // since whatever is returned from our API will simply replace what was in our state from the dispatch payload, all we have to do is return the dispatch payload of authors here
