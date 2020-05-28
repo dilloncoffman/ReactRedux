0ded42232f5c6563bfe546024b36103ed9001b25
Dillon Coffman <tug40394@temple.edu>
2020-05-28 11:56:58 -0400

    Configured Jest test runner

---
+++ tools/fileMock.js
@@ -0,0 +1,2 @@
+// Mocks file imports for Jest. As suggesstedd by https://jestjs.io/docs/en/webpack
+module.exports = 'test-file-stub';
---
+++ tools/styleMock.js
@@ -0,0 +1,3 @@
+// Mocks CSS imports for Jest. As suggested by https://jestjs.io/docs/en/webpack
+// Ignore CSS imports if Jest sees you importing CSS files
+module.exports = {};
