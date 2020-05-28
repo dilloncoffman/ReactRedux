0e9a65a58d6f2d516012adf4658830d969244160
Dillon Coffman <tug40394@temple.edu>
2020-05-28 08:50:18 -0400

    In courseReducer.js, added reducers for CREATE_COURSE_SUCCESS and UPDATE_COURSE_SUCCESS

--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -3,8 +3,12 @@ import initialState from '../initialState';
 
 export default function courseReducer(state = initialState.courses, action) {
   switch (action.type) {
-    case types.CREATE_COURSE:
+    case types.CREATE_COURSE_SUCCESS:
       return [...state, { ...action.course }];
+    case types.UPDATE_COURSE_SUCCESS:
+      return state.map(
+        (course) => (course.id === action.course.id ? action.course : course) // when you find the course with the id that was just changed, replace it in the array, this is important because map() does not mutate state, it returns a new array
+      );
     case types.LOAD_COURSES_SUCCESS:
       return action.courses; // since whatever is returned from our API will simply replace what was in our state, all we have to do is return the dispatch payload of courses here
     default:
